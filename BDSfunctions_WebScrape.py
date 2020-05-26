2049cee73acb3e119f84a1e59998efcf806c045f
Emily Webb <eawebb1@asu.edu>
2020-05-26 14:29:01 -0700

    added functions to extract diet section of html in website

--- BDSfunctions_WebScrape.py
+++ BDSfunctions_WebScrape.py
@@ -1,10 +1,18 @@
 """
-Scrapy spider for logging in to Birds of the World via AOS access
+Scrapy spider for...
+1. logging in to Birds of the World via AOS access
+2. going to "Diet and Foraging" section of given set of species pages
+3. extracting only the "Diet" subsection as txt file output
 """
 
 import scrapy
 from scrapy.http import FormRequest
-from scrapy.utils.response import open_in_browser
+from scrapy.http import Request
+#from scrapy.utils.response import open_in_browser
+
+base_url_start = "https://birdsoftheworld-org.proxy.birdsoftheworld.org/bow/species/"
+base_url_end = "/cur/foodhabits"
+pages = ["comyel"]
 
 class DietsSpider(scrapy.Spider):
     name = "diets"
@@ -21,10 +29,21 @@ class DietsSpider(scrapy.Spider):
                 "url": "https://birdsoftheworld.org", 
                 "aosuser": aosuser, 
                 "pass": password
-                }, callback = self.start_scraping)
+                }, callback = self.after_login)
     
-    def start_scraping(self, response):
-        open_in_browser(response)
+    def after_login(self, response):
+        for page in pages:
+            yield Request(
+                    url = base_url_start + page + base_url_end, callback = self.action)
+        
+    
+    def action(self, response):
+        #open_in_browser(response)
+        t = response.xpath("/html/body/div/main/div[2]/div/div/div[2]/section[2]").extract()
+        with open("output.txt", "a") as f:
+            for i, line in enumerate(t):
+                f.write(line + "\n")
+
 
 
 
